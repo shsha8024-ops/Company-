1fee547644bc0f3c691de81b2fa565a6baa1d441
shsha <shsha8024@gmail.com>
2025-10-16 15:58:14 +0300

    Update service worker cache and asset list

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,5 @@
-const CACHE='ghadeer-pin-cache-v1';
-const ASSETS=['./','./index.html','./assets/css/theme.css','./assets/js/crypto.js','./assets/js/app.js'];
+const CACHE='ghadeer-auth-cache-v2';
+const ASSETS=['./','./index.html','./assets/css/theme.css','./assets/js/Crypto.js','./assets/js/app.js'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
-self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)))})
+self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))))});
+self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)))});
